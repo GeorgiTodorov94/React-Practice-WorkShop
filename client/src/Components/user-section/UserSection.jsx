6d33e6b4666db382a65174b34f7661255decc321
Your Name <you@example.com>
2024-07-10 10:32:39 +0300

    Implemented Fetching Users from server
    Used IIFE

--- client/src/Components/user-section/UserSection.jsx
+++ client/src/Components/user-section/UserSection.jsx
@@ -1,6 +1,25 @@
 import SearchForm from "../search/Search";
 import UserList from "./user-list/UserList";
+import { useState, useEffect } from "react";
+
+const baseUrl = 'http://localhost:3030/jsonstore'
+
+
 export default function UserSection() {
+
+    const [users, setUsers] = useState();
+
+
+    useEffect(() => {
+        (async function getUsers() {
+
+            const response = await fetch(`${baseUrl}/users`);
+            const result = await response.json();
+
+        })();
+
+    }, [])
+
     return (
         <>
             <section className="card users-container">
